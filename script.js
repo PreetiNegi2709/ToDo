fa415453de3c94e0091819ab72b6df1ee418667f
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-02-24 23:07:26 +0530

    second push

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-let key,val;
-let todoArray=[];
-const content = document.querySelector(".todoList")
+let key, val;
+let todoArray = [];
+let delete_btn;
+const content = document.querySelector(".todoList");
+store_btn.addEventListener("click", function (e) {
+  key = document.getElementById("inp1").value;
+  val = document.getElementById("inp2").value;
+  todoArray = [...todoArray, { title: key, des: val }];
+  const list = todoArray.map((rem,index) => {
+    return `<li class="listElem">
+        <h3> ${rem.title} :  </h3>
+        <p> ${rem.des} </p> 
+        <button type="button" class="btn_cross">X</button></li>`;
+}).join(`<br>`);
+// array ko string conv krte h to , inset hota h; map by def string me conv krta h
 
-store_btn.addEventListener("click",function(e){
-    key = document.getElementById("inp1").value
-    val = document.getElementById("inp2").value
-    todoArray = [...todoArray,{title:key,des:val}]
-    // console.log(todoArray)
-    // console.log(todoArray[0])
-    // console.log(todoArray[1])
-    const list = todoArray.map((rem,index)=>{
-        // console.log("hi")
-        return  `<div class="listElem"><h3> ${rem.title} :  </h3>
-        <p> ${rem.des} </p> <button type="button" class="btn_cross"> X </button> </div> `
-    })
-    content.innerHTML= list
-    // todo.innerHTML= list
+content.innerHTML = list; 
 
-    // let delRem = document.getElementsByClassName("btn_cross")
-    // delRem.addEventListener('click',function(e){
-    //     alert("ok")
-    // })
-
-    let delRem = document.querySelector(".btn_cross")
-    delRem.addEventListener('click',function(e){
-        alert("ok")
-    })
-
-})
-
-
-
-
-
-
-
-
-
-
-
+  delete_btn = document.getElementsByClassName("btn_cross");
+  for (let i = 0; i < delete_btn.length; i++) {
+    delete_btn[i].addEventListener("click", function () {
+    //   alert("item deleted");
+      var div = this.parentElement;
+      div.style.display="none";
+    });
+  }
+});
